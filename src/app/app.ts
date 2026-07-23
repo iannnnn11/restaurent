ab4a807c900c1ea1737b6c30bc492acbfe02a9ea
iannnnn11 <ianhoney011@gmail.com>
2026-07-23 18:59:48 +0530

    started signup page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './components/login/login';
+import { Signup } from './components/signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Login,Signup],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
