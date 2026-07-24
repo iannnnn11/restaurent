d0f05c7bf1431d7940f4608b4097daa5418e0bf4
iannnnn11 <ianhoney011@gmail.com>
2026-07-24 18:12:43 +0530

    added 3 more components for navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Login } from './components/login/login';
 import { Signup } from './components/signup/signup';
 import { Dashboard } from './components/dashboard/dashboard';
 import { Menu } from './components/menu/menu';
+import { Orders } from './components/orders/orders';
+import { Cart } from './components/cart/cart';
+import { Categories } from './components/categories/categories';
 
 export const routes: Routes = [
   {
@@ -25,5 +28,17 @@ export const routes: Routes = [
   {
     path: 'menu',
     component: Menu
+  },
+  {
+    path: 'orders',
+    component: Orders
+  },
+  {
+    path: 'cart',
+    component: Cart
+  },
+  {
+    path: 'categories',
+    component: Categories
   }
 ];
---
+++ src/app/components/orders/orders.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-orders',
+  imports: [RouterLink],
+  templateUrl: './orders.html',
+  styleUrl: './orders.css',
+})
+export class Orders {}
